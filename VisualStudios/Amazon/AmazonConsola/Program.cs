f3d6dfa4b1d64636ce0d4ebc52ae6c7172e16b1d
angelcr25 <angelsamuelcr25@gmail.com>
2019-10-29 10:00:58 -0300

    creacion de las clases
    agruegue las clases y un proyecto de consola

---
+++ VisualStudios/Amazon/AmazonConsola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AmazonConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
