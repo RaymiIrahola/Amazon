82b5ffa1176c417af1a405809db9863f56f527ea
RaymiIrahola <raymiirahola@gmail.com>
2019-10-29 11:11:23 -0300

    Creacion de atributos en las clases

--- VisualStudios/Amazon/Amazon/Envio.cs
+++ VisualStudios/Amazon/Amazon/Envio.cs
@@ -4,7 +4,34 @@ using System.Text;
 
 namespace Amazon
 {
-    class Envio
+    public class Envio
     {
+        public UsuarioDestinatario destinatario { get; set; }
+
+        public string direccionDeDestino { get; set; }
+
+        public  DateTime fechaEnvio { get; set; }
+
+        public DateTime fechaLlegada { get; set; }
+
+        public int idEnvio { get; set; }
+
+        public int numEnvio { get; set; }
+
+        public Paquete paquete { get; set; }
+
+        public UsuarioRemitente remitente { get; set; }
+
+        public TipodeEnvio tipoEnvio { get; set; }
+
+        public float CalcularEnvioDePeso(Paquete paquete)
+        {
+
+        }
+
+        public DateTime CalcularFechaLlegada(DateTime fechaEnvio)
+        {
+
+        }
     }
 }
