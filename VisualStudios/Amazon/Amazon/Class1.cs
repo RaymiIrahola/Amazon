5e9edb9780569b9ec0986d5c937fbcc552620bf9
RaymiIrahola <raymiirahola@gmail.com>
2019-10-29 09:26:11 -0300

    Agregacion de los "using" en el proyecto

--- VisualStudios/Amazon/Amazon/Class1.cs
+++ VisualStudios/Amazon/Amazon/Class1.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+
+//Using esenciales
+
 
 namespace Amazon
 {
