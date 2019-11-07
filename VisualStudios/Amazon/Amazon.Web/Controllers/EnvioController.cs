03ebc71eae996952ce340f29d0530ca7c98fd067
gustavo1020 <gustavolores15@gmail.com>
2019-11-07 17:19:56 -0300

    hfh
    rhth

--- VisualStudios/Amazon/Amazon.Web/Controllers/EnvioController.cs
+++ VisualStudios/Amazon/Amazon.Web/Controllers/EnvioController.cs
@@ -25,9 +25,7 @@ namespace Amazon.Web.Controllers
         [HttpGet]
         public IActionResult Agregar()
         {
-            /*
-               Retorna la vista con nombre "Agregar" la cual permite la carga de datos de un empleado
-            */
+            
             return View("Agregar");
         }
 
@@ -36,17 +34,10 @@ namespace Amazon.Web.Controllers
         {
             var envios = db.Context.GetCollection<Envio>("amazon");
 
-            /*
-                Insert: método propio de la biblioteca LiteDB, permite guardar un objeto de tipo Empleado
-                en la base de datos
-             */
+            
             envios.Insert(envio);
 
-            /*
-                Redirecciona la vista "Index" con el contenido de la lista empleados con tipo de datos
-                IEnumerable, se usa este método de redireccionamiento en vez de View() para que cambie 
-                la refencia en la barra de direcciones
-             */
+          
             return RedirectToAction("Index", envios.FindAll());
         }
 
@@ -54,11 +45,7 @@ namespace Amazon.Web.Controllers
         {
             var envios = db.Context.GetCollection<Envio>("nomina");
 
-            /*
-               Delete: método propio de la biblioteca LiteDB, permite eliminar un objeto de tipo Empleado
-               en la base de datos, en este caso como se tiene el objeto, pero si el Id y como este es unico
-               procedo a borrar todos los objetos que posean el mismo Id.
-            */
+           
             envios.Delete(x => x.id == id);
 
             return RedirectToAction("Index", envios.FindAll());
