970a39a6efd424ba26a01f1c918d19d07bbc9720
angelcr25 <angelsamuelcr25@gmail.com>
2019-11-12 09:34:58 -0300

    Agregue la clase tipo de envio

---
+++ VisualStudios/Amazon/Amazon/TipodeEnvio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amazon
+{
+    public class TipodeEnvio
+    {
+        public string codigp { get; set; }
+        public int costoDeEnvio { get; set; }
+        public DateTime duracionDeEnvio { get; set; }
+        public int idTipodeEnvio { get; set; }
+        public int numTipodeEnvio { get; set; }
+    }
+}
